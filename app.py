bae59363d0169ce5285e87caef95ac979bca7937
SHETANSHU <pandeyshetanshu@gmail.com>
2026-02-17 14:55:48 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,19 @@
 import streamlit as st
-import pandas as pd
 from model import recommend
 
-hollywood = pd.read_csv("hollywood.csv")
-bollywood = pd.read_csv("bollywood.csv")
+st.title("🎬 Movie Recommendation System")
 
-movies = pd.concat([hollywood, bollywood])
+st.write("Get movie recommendations based on genre.")
 
-st.title("🎬 Pandey Recommendations")
-st.write("Bollywood + Hollywood Movie Recommendation System")
-
-selected_movie = st.selectbox(
-    "Select a Movie",
-    movies['title'].values
-)
+movie_name = st.text_input("Enter a movie name:")
 
 if st.button("Recommend"):
-    recommendations = recommend(selected_movie)
-    for movie in recommendations:
-        st.write(movie)
+    if movie_name.strip() == "":
+        st.warning("Please enter a movie name.")
+    else:
+        results = recommend(movie_name)
+
+        st.subheader("Recommended Movies:")
+        for movie in results:
+            st.write("•", movie)
+
