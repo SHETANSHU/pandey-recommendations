032e08833c05886e2216d95e4bc8d38f4aadb273
SHETANSHU <pandeyshetanshu@gmail.com>
2026-02-17 15:09:36 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,18 @@
 import streamlit as st
 from model import recommend
 
-st.title("🎬 Movie Recommendation System")
+st.set_page_config(page_title="Pandey Recommendations")
 
-st.write("Get movie recommendations based on genre.")
+st.title("🎬 Pandey Movie Recommendation System")
+st.write("Developed by Shetanshu Pandey")
 
 movie_name = st.text_input("Enter a movie name:")
 
 if st.button("Recommend"):
-    if movie_name.strip() == "":
-        st.warning("Please enter a movie name.")
-    else:
+    if movie_name:
         results = recommend(movie_name)
-
         st.subheader("Recommended Movies:")
         for movie in results:
-            st.write("•", movie)
-
+            st.write(movie)
+    else:
+        st.warning("Please enter a movie name.")
